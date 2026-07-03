765c79dc4a3af6c8ec690d46081616201d37a198
RedFlameKen <nettolacaba@gmail.com>
2026-07-03 14:00:32 +0800

    feat: add more client origins

--- ScholarsTavernServer/settings.py
+++ ScholarsTavernServer/settings.py
@@ -32,7 +32,9 @@ ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "https://scholarstavern.netlify.app",
 ]
 
 # Application definition
